17953eb3ea823efe1d4013ad9dce86edbe06568c
*Personal* <premkumar.cherupally@gmail.com>
2023-05-05 14:33:32 +0530

    FirstCommit

--- src/test/java/com/example/BookMyTrain/BookMyTrainApplicationTests.java
+++ src/test/java/com/example/BookMyTrain/BookMyTrainApplicationTests.java
@@ -24,38 +24,39 @@ import java.util.List;
 
 @SpringBootTest
 class BookMyTrainApplicationTests {
-	@Autowired
-	BookSerive bookSerive;
-EntryStation entryStation=new EntryStation("HYD");
-EntryStation entryStation1=new EntryStation("HYD");
-Station station=new Station();
+    @Autowired
+    BookSerive bookSerive;
+    EntryStation entryStation = new EntryStation("HYD");
+    EntryStation entryStation1 = new EntryStation("HYD");
+    Station station = new Station();
 
-JourneyDetails journeyDetails=new JourneyDetails("dsd", StationConverter.DtoToEntity(entryStation),StationConverter.DtoToEntity(entryStation1),StationConverter.DtoToEntity(entryStation),LocalTime.now());
-    EntryTrain train =new EntryTrain("VIT","venkatadri",entryStation,entryStation1,LocalTime.now(),new Date(),LocalTime.MIN, Duration.ofHours(5));
+    JourneyDetails journeyDetails = new JourneyDetails("dsd", StationConverter.DtoToEntity(entryStation), StationConverter.DtoToEntity(entryStation1), StationConverter.DtoToEntity(entryStation), LocalTime.now());
+    EntryTrain train = new EntryTrain("VIT", "venkatadri", entryStation, entryStation1, LocalTime.now(), new Date(), LocalTime.MIN, Duration.ofHours(5));
 
-	EntryUserDto userDto=new EntryUserDto("prem","32456","dfsadsad222","premkumar.cherupally@gmail.com","psjs");
-	List<Seat> seatList=new ArrayList<>(Arrays.asList(new Seat("aaa","Sleeper",1000,false),new Seat("aa","Sleeper",100,false),new Seat("a","Sleeper",10,false)));
-	EntryBookData entryBook =new EntryBookData(userDto,journeyDetails,seatList,train);
+    EntryUserDto userDto = new EntryUserDto("prem", "32456", "dfsadsad222", "premkumar.cherupally@gmail.com", "psjs");
+    List<Seat> seatList = new ArrayList<>(Arrays.asList(new Seat("aaa", "Sleeper", 1000, false), new Seat("aa", "Sleeper", 100, false), new Seat("a", "Sleeper", 10, false)));
+    EntryBookData entryBook = new EntryBookData(userDto, journeyDetails, seatList, train);
 
 
+    @Test
+    void testBookService() throws TheTrainAsStarted {
+        TicketInformation ticketInformation = new TicketInformation();
 
-	@Test
-	void testBookService() throws TheTrainAsStarted {
-		TicketInformation ticketInformation=new TicketInformation();
+        Assertions.assertEquals(ticketInformation, bookSerive.bookTrain(entryBook));
+    }
 
-		Assertions.assertEquals(ticketInformation,bookSerive.bookTrain(entryBook));
-	}
-	@Test
-	void testBookServiceWaittingList() throws TheTrainAsStarted {
-		TicketInformation ticketInformation=new TicketInformation();
+    @Test
+    void testBookServiceWaittingList() throws TheTrainAsStarted {
+        TicketInformation ticketInformation = new TicketInformation();
 
-		Assertions.assertEquals(ticketInformation,bookSerive.bookTrain(entryBook));
-	}
-	@Test
-	void testAddStation() {
-		TicketInformation ticketInformation=new TicketInformation();
+        Assertions.assertEquals(ticketInformation, bookSerive.bookTrain(entryBook));
+    }
 
-		Assertions.assertEquals(ticketInformation,bookSerive.bookTrain(entryBook));
-	}
+    @Test
+    void testAddStation() {
+        TicketInformation ticketInformation = new TicketInformation();
+
+        Assertions.assertEquals(ticketInformation, bookSerive.bookTrain(entryBook));
+    }
 
 }
